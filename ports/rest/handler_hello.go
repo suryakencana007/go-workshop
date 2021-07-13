1896be08508e6a92848d0ce3d14fcaf867832e7e
Ivan Octaviano <ivanoctaviano@warungpintar.co>
2021-07-13 13:39:12 +0700

    feat: add new endpoint status-ok, status-accepted, and upload

--- ports/rest/handler_hello.go
+++ ports/rest/handler_hello.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/go-chi/chi/v5"
 	"net/http"
+	"strconv"
 
 	pkgHttp "go-workshop/pkg/http"
 )
@@ -14,6 +15,9 @@ type Hello struct {
 
 func (h *Hello) Register(ctx context.Context, router chi.Router) {
 	router.Get("/hello", h.Hello)
+	router.Get("/status-ok", h.StatusOk)
+	router.Get("/status-accepted", h.StatusAccepted)
+	router.Post("/upload", h.Upload)
 }
 
 func (h *Hello) Hello(w http.ResponseWriter, r *http.Request) {
@@ -21,5 +25,32 @@ func (h *Hello) Hello(w http.ResponseWriter, r *http.Request) {
 
 	_ = pkgHttp.RequestJSONBody(w, r, http.StatusOK, map[string]interface{}{
 		"Message": "Hello",
+	})
+}
+
+func (h *Hello) StatusOk(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	_ = pkgHttp.RequestJSONBody(w, r, http.StatusOK, map[string]interface{}{
+		"Status": strconv.Itoa(http.StatusOK), 
+		"Handler": "handler_status_ok",
+	})
+}
+
+func (h *Hello) StatusAccepted(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	_ = pkgHttp.RequestJSONBody(w, r, http.StatusAccepted, map[string]interface{}{
+		"Status": strconv.Itoa(http.StatusAccepted), 
+		"Handler": "handler_status_accepted",
+	})
+}
+
+func (h *Hello) Upload(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	_ = pkgHttp.RequestJSONBody(w, r, http.StatusAccepted, map[string]interface{}{
+		"Status": strconv.Itoa(http.StatusAccepted), 
+		"Handler": "handler_upload",
 	})
 }

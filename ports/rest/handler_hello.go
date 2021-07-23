9c31d8cae24e7dc3cd43d7e2d28833ff3beb8f80
suryakencana007 <nanang.jobs@gmail.com>
2021-07-23 11:44:02 +0700

    feat: handler upload file

--- ports/rest/handler_hello.go
+++ ports/rest/handler_hello.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/go-chi/chi/v5"
-	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -55,40 +55,53 @@ func (h *Hello) Upload(w http.ResponseWriter, r *http.Request) {
 		"Status":  strconv.Itoa(http.StatusAccepted),
 		"Handler": "Upload",
 	}
-	if r.Method == http.MethodPost {
-		if err := r.ParseMultipartForm(32 << 20); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		f, fh, err := r.FormFile("upload-file")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		defer f.Close()
-
-		dir, err := os.Getwd()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		fileLocation := filepath.Join(dir, "files", fh.Filename)
-		targetFile, err := os.OpenFile(fileLocation, os.O_WRONLY|os.O_CREATE, 0666)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		defer targetFile.Close()
-
-		if _, err := io.Copy(targetFile, f); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	} else {
+	fmt.Println("upload")
+	if r.Method != http.MethodPost {
 		http.Error(w, fmt.Errorf("%s method not allowed", r.Method).Error(), http.StatusMethodNotAllowed)
 		return
 	}
+	if err := r.ParseMultipartForm(32 << 20); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	f, fh, err := r.FormFile("upload-file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer f.Close()
+
+	fmt.Printf("Uploaded File: %+v\n", fh.Filename)
+	fmt.Printf("File Size: %+v\n", fh.Size)
+	fmt.Printf("MIME Header: %+v\n", fh.Header)
+
+	dir, err := os.Getwd()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fileLocation := filepath.Join(dir, "files")
+	fl := filepath.FromSlash(fileLocation)
+	// Create a temporary file within our temp-images directory that follows
+	// a particular naming pattern
+	tempFile, err := ioutil.TempFile(fl, fmt.Sprintf("*.%s",  filepath.Ext(fh.Filename)))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer tempFile.Close()
+
+	// read all of the contents of our uploaded file into a
+	// byte array
+	fileBytes, err := ioutil.ReadAll(f)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// write this byte array to our temporary file
+	tempFile.Write(fileBytes)
 
 	_ = pkgHttp.RequestJSONBody(w, r, http.StatusAccepted, payload)
 }

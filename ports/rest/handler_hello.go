95be8e1990010f187e1b9a67bb8b40ced6ecf810
ungkas <satryanda.pamungkas@gmail.com>
2021-07-28 12:02:14 +0700

    [IMP] Parse multiform value

--- ports/rest/handler_hello.go
+++ ports/rest/handler_hello.go
@@ -2,7 +2,11 @@ package rest
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 
@@ -44,9 +48,78 @@ func (h *Hello) Accepted(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Hello) Upload(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("starting upload...")
+
+	// HTTP method check
+	if r.Method != http.MethodPost {
+		http.Error(w, fmt.Errorf("%s method not allowed", r.Method).Error(), http.StatusMethodNotAllowed)
+		return
+	}
+
+	// 32MB max upload size
+	err := r.ParseMultipartForm(32 << 20)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// get value
+	v := r.PostFormValue("file-name")
+
+	// get file
+	f, fh, err := r.FormFile("upload-file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// close the file when finished
+	defer f.Close()
+
+	// file info log
+	fmt.Printf("File Name\t: %+v\n", v)
+	fmt.Printf("Uploaded File\t: %+v\n", fh.Filename)
+	fmt.Printf("File Size\t: %+v\n", fh.Size)
+	fmt.Printf("MIME Header\t: %+v\n", fh.Header)
+
+	// get current path
+	dir, err := os.Getwd()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fileLocation := filepath.Join(dir, "files")
+	fl := filepath.FromSlash(fileLocation)
+	// Create a temporary file within our temp-images directory that follows
+	// a particular naming pattern
+	tempFile, err := ioutil.TempFile(fl, fmt.Sprintf("*.%s", filepath.Ext(fh.Filename)))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer tempFile.Close()
+
+	// read all of the contents of our uploaded file into a
+	// byte array
+	fileBytes, err := ioutil.ReadAll(f)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// write this byte array to our temporary file
+	tempFile.Write(fileBytes)
+
+	fmt.Println("upload finished")
+
 	w.Header().Set("Content-Type", "application/json")
-	pkgHttp.RequestJSONBody(w, r, http.StatusAccepted, map[string]interface{}{
+	response := map[string]interface{}{
 		"Status":  "202",
-		"Handler": "REQUEST_SUCCESS",
-	})
+		"Handler": "UPLOAD_SUCCESS",
+	}
+	pkgHttp.RequestJSONBody(w, r, http.StatusAccepted, response)
+
 }
